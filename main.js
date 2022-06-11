0c319a584e35a4f9edbc6a6f66f7f65432c52877
NatiTesfay <netaneltes66@gmail.com>
2022-06-12 00:23:33 +0300

    1

---
+++ main.js
@@ -0,0 +1,28 @@
+var box_witdh=document.getElementById('container');
+var container = 0;
+var intervalId;
+function startkBtn() {
+ intervalId = setInterval(() => {
+    box_witdh.style.height = container++ + "px"
+    
+}, 10);
+}
+function stopBtn(){
+    clearInterval(intervalId);
+}
+
+var intervalId;
+function handlerFunction(e) {
+    if ( e.key == "Enter") {
+        alert('good');
+    }
+    
+}
+
+
+
+
+
+
+
+
