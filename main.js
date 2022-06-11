d06c8d9db63911c82cff884001dd88b6255a1b67
NatiTesfay <netaneltes66@gmail.com>
2022-06-12 00:54:29 +0300

    1

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-var box_witdh=document.getElementById('container');
-var container = 0;
-var intervalId;
-function startkBtn() {
- intervalId = setInterval(() => {
-    box_witdh.style.height = container++ + "px"
+// var box_witdh=document.getElementById('container');
+// var container = 0;
+// var intervalId;
+// function startkBtn() {
+//  intervalId = setInterval(() => {
+//     box_witdh.style.height = container++ + "px"
     
-}, 10);
-}
-function stopBtn(){
-    clearInterval(intervalId);
-}
+// }, 10);
+// }
+// function stopBtn(){
+//     clearInterval(intervalId);
+// }
 
-var intervalId;
-function handlerFunction(e) {
-    if ( e.key == "Enter") {
-        alert('good');
-    }
+// var intervalId;
+// function handlerFunction(e) {
+//     if ( e.key == "Enter") {
+//         alert('good');
+//     }
     
+// }
+
+function clickChange() {
+    setInterval(() => {
+        chancge_col.style.background = "red"
+    }, 1000);
 }
 
 
